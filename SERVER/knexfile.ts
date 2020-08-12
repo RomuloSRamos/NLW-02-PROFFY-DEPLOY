ee196929f9d10742f58763c28165da31195327d3
Romulo S. Ramos <rsromulo@msn.com>
2020-08-12 16:34:37 -0300

    :construction::arrow_up:DOTENV:heavy_check_mark:

--- SERVER/knexfile.ts
+++ SERVER/knexfile.ts
@@ -1,13 +1,14 @@
 import path from 'path';
+import 'dotenv/config';
 
 module.exports = {
     client:'pg',
     
     connection: {
-        host: 'localhost',
-		user: 'postgres',
-		password: 'docker',
-		database: 'proffy',
+        host : process.env.PG_HOST,
+        user : process.env.PG_USER,
+        password : process.env.PG_PASSWORD,
+        database : process.env.PG_DATABASE,
     },
 
     migrations:{
--- SERVER/src/server.ts
+++ SERVER/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import routes from './routes';
 import cors from 'cors';
+import 'dotenv/config';
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3333);
+
+app.listen(process.env.PORT || 3333);
